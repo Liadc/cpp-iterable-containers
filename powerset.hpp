a02c5c45b6ab94ab589830ba6b22aa4443ccb5ca
Liad Cohen - Ubuntu VM <liad.code@gmail.com>
2019-05-06 15:37:25 +0300

    added class files

---
+++ powerset.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+namespace itertools {
+    template<typename T> class powerset{
+        
+
+    };
+}
---
+++ range.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+namespace itertools {
+    template<typename T> class range{
+        private:
+
+        public:
+
+    };
+}
