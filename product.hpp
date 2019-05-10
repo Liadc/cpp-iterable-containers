743ed1cc922b8b2d3d70a72ce48c922b912262ff
Timor Sharabi <xposionn@gmail.com>
2019-05-10 16:01:07 +0300

    description has beed added

--- product.hpp
+++ product.hpp
@@ -11,60 +11,57 @@ using namespace std;
 namespace itertools {
     template<typename T,typename V> class product{
         private:
-        T firstProduct;
-        V secondProduct;
+            T firstProduct;
+            V secondProduct;
         public:
-        product(T a, V b):firstProduct(a),secondProduct(b){}
+            product(T a, V b):firstProduct(a),secondProduct(b){}
         class iterator{
             private:
-            typename T::iterator firstItr;
-            typename T::iterator firstItr_end;
-            typename V::iterator secondItr;
-            typename V::iterator secondItr_end;
-            typename V::iterator secondItrSave;
+                typename T::iterator firstItr;
+                typename T::iterator firstItr_end;
+                typename V::iterator secondItr;
+                typename V::iterator secondItr_end;
+                typename V::iterator secondItrSave;
             public:
-            //constructor
-            iterator(typename T::iterator first,typename T::iterator fend,typename V::iterator second,typename V::iterator send): 
-            firstItr(first),secondItrSave(second),secondItr(second),firstItr_end(fend),secondItr_end(send){}
+                //constructor
+                iterator(typename T::iterator first,typename T::iterator fend,typename V::iterator second,typename V::iterator send): 
+                firstItr(first),secondItrSave(second),secondItr(second),firstItr_end(fend),secondItr_end(send){}
 
             
-            //operators: to behave like iterator, we need: ++(increment) , *(access) , !=(not equal)
-            iterator& operator ++(){
-                if(secondItr != secondItr_end){
-                    if(++secondItr != secondItr_end){
-                    }else{
-                        if(++firstItr!=firstItr_end){
-                        secondItr = secondItrSave;
+                //operators: to behave like iterator, we need: ++(increment) , *(access) , !=(not equal)
+                iterator& operator ++(){
+                    if(secondItr != secondItr_end){
+                        if(++secondItr != secondItr_end){
+                        }else{
+                            if(++firstItr!=firstItr_end){
+                            secondItr = secondItrSave;
+                            }
                         }
                     }
+                    return *this;
                 }
-                return *this;
-            }
-            auto operator *(){
-                return couple(*firstItr,*secondItr);
-            }
+                auto operator *(){ //will return a "couple" of the iterators
+                    return couple(*firstItr,*secondItr);
+                }
 
 
-            bool operator==(iterator& other) const
+                bool operator==(iterator& other) const
+                    {
+                    return (this->firstItr == other.firstItr && this->secondItr == other.secondItr);
+                    }
+
+                bool operator!=(iterator& other) const
                 {
-                return (this->firstItr == other.firstItr && this->secondItr == other.secondItr);
+                    return !(*this==other);
                 }
+            };
 
-            bool operator!=(iterator& other) const
-            {
-                return !(*this==other);
+            //begin and end 
+            iterator begin(){
+                return product<T,V>::iterator(firstProduct.begin(),firstProduct.end(),secondProduct.begin(),secondProduct.end());
             }
-        };
-
-        //begin and end 
-        iterator begin(){
-            return product<T,V>::iterator(firstProduct.begin(),firstProduct.end(),secondProduct.begin(),secondProduct.end());
-        }
-        iterator end(){
-            return product<T,V>::iterator(firstProduct.end(),firstProduct.end(),secondProduct.end(),secondProduct.end());
-        }
-
-        private:
-
+            iterator end(){
+                return product<T,V>::iterator(firstProduct.end(),firstProduct.end(),secondProduct.end(),secondProduct.end());
+            }
     };
 }
