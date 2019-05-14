55c5e8232f8edc088da48d30be3cf269f5a81a82
Liad Cohen <liad.code@gmail.com>
2019-05-14 14:40:50 +0300

    added test combinations for range,chains

--- Test.cpp
+++ Test.cpp
@@ -18,7 +18,7 @@ using std::cout, std::endl;
 using namespace itertools;
 
 template<typename Iterable>
-string iterable_to_string(const Iterable& iterable){
+string toString(const Iterable& iterable){
  stringstream ostr;
  for(decltype(*(iterable.begin())) i: iterable){
    ostr << i << ",";
@@ -38,6 +38,8 @@ int main() {
     range int2 = range(6,7);
     range double1 = range(0.1,4.1);
     range char1 = range('a','d');
+    range oneChar = range('a','b');
+    range hugeRangeCheckBytes = range(0,10000);
     string str = string("our test");
         //Chains:
     chain c1 = chain(int1,int2);
@@ -52,7 +54,6 @@ int main() {
     string int2Ans = "6,";
     string double1Ans = "0.1,1.1,2.1,3.1,";
     string char1Ans = "a,b,c,";
-
       //Chain answers:
 
     string c1Ans = "-1,0,1,2,6,";
@@ -63,15 +64,17 @@ int main() {
     string chainAnswerArr[2] = {c1Ans,c2Ans};
       cout<< "before tests" << endl;
       tc.setname("Ranges:")
-      .CHECK_EQUAL(iterable_to_string(int1),rangeAnswerArr[0])
-      .CHECK_EQUAL(iterable_to_string(int2),rangeAnswerArr[1])
-      .CHECK_EQUAL(iterable_to_string(double1),rangeAnswerArr[2])
-      .CHECK_EQUAL(iterable_to_string(char1),rangeAnswerArr[3])
+      .CHECK_EQUAL(toString(int1),rangeAnswerArr[0])
+      .CHECK_EQUAL(toString(int2),rangeAnswerArr[1])
+      .CHECK_EQUAL(toString(double1),rangeAnswerArr[2])
+      .CHECK_EQUAL(toString(char1),rangeAnswerArr[3])
+      .CHECK_EQUAL(toString(range('a','b')),string("a,")) //1 char
 
       .setname("Chains:")
-      .CHECK_EQUAL(iterable_to_string(c1),chainAnswerArr[0])
-      .CHECK_EQUAL(iterable_to_string(c2),chainAnswerArr[1]) 
-      .CHECK_EQUAL(iterable_to_string(double1),rangeAnswerArr[2])
+      .CHECK_EQUAL(toString(c1),chainAnswerArr[0])
+      .CHECK_EQUAL(toString(c2),chainAnswerArr[1]) 
+      .CHECK_EQUAL(toString(chain(oneChar,string("b"))),string("a,b,"))
+      .CHECK_EQUAL(toString(chain(chain(oneChar,string("b")),string("c"))),string("a,b,c,")) //chain of chains of ranges (oneChar is range of 1 char)
 
       .setname("Zips:")
       
@@ -83,7 +86,7 @@ int main() {
 
       .print();
 
-      cout<< "after tests" << endl;
+      cout<< "After tests: " << endl;
   
   cout << "You have " << tc.right() << " right answers and " << tc.wrong() << " wrong answers so your grade is " << tc.grade() << ". Great!" << endl;
 }
