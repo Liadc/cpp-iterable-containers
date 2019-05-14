8ea64064e531888d2bf135cf12d167567050bfa2
Liad Cohen <liad.code@gmail.com>
2019-05-14 15:37:47 +0300

    added powerset,chains, zip tests

--- Test.cpp
+++ Test.cpp
@@ -39,14 +39,21 @@ int main() {
     range double1 = range(0.1,4.1);
     range char1 = range('a','d');
     range oneChar = range('a','b');
-    range hugeRangeCheckBytes = range(0,10000);
+    range hugeRangeCheckBytes = range(0,10000000); //should give max of 8 bytes, or anything below 100bytes will be reasonable.
+    //if it takes too much time to compute and construct the object, something is wrong!
     string str = string("our test");
         //Chains:
     chain c1 = chain(int1,int2);
     chain c2 = chain(str,char1);
     chain c3 = chain(int1,double1);
 
-
+    std::ostringstream zip1, zip2, zip3, zip4, zip5;
+      for (auto zipped : zip(range(-1, 1), range(-1, 1)))
+        zip1 << zipped << "<>";
+      string zip1Ans = "-1,-1<>0,0<>";
+      // for (auto zipped : zip(range('a','c'), range(-1.1,1.0)))
+      //   zip1 << zipped << "<>";
+      // // tc.CHECK_EQUAL(zip1.str(), "-1,-1<>0,0<>");
 
     //Answers
       //Range answers:
@@ -62,7 +69,11 @@ int main() {
     //Arrayed Answers
     string rangeAnswerArr[4] = {int1Ans,int2Ans,double1Ans,char1Ans}; 
     string chainAnswerArr[2] = {c1Ans,c2Ans};
-      cout<< "before tests" << endl;
+      //tc.setname("Container Sizes: ")
+      //Badkan does not have less/greater than function. this is manual test: Container Sizes
+      cout << "Size of huge range, in bytes: " << sizeof(hugeRangeCheckBytes) << endl; //should give max of 8 bytes, or anything below 100bytes will be reasonable.
+      //also, if it takes too much time to compute and construct the object, something is wrong!
+      
       tc.setname("Ranges:")
       .CHECK_EQUAL(toString(int1),rangeAnswerArr[0])
       .CHECK_EQUAL(toString(int2),rangeAnswerArr[1])
@@ -77,16 +88,21 @@ int main() {
       .CHECK_EQUAL(toString(chain(chain(oneChar,string("b")),string("c"))),string("a,b,c,")) //chain of chains of ranges (oneChar is range of 1 char)
 
       .setname("Zips:")
-      
+      .CHECK_EQUAL(zip1.str(), zip1Ans)
+
+
       .setname("Products:")
-      
+
 
       .setname("Powersets:")
-      
+      .CHECK_EQUAL(toString(powerset(range(-3,2))),string("{},{-3},{-2},{-3,-2},{-1},{-3,-1},{-2,-1},{-3,-2,-1},{0},{-3,0},{-2,0},{-3,-2,0},{-1,0},{-3,-1,0},{-2,-1,0},{-3,-2,-1,0},{1},{-3,1},{-2,1},{-3,-2,1},{-1,1},{-3,-1,1},{-2,-1,1},{-3,-2,-1,1},{0,1},{-3,0,1},{-2,0,1},{-3,-2,0,1},{-1,0,1},{-3,-1,0,1},{-2,-1,0,1},{-3,-2,-1,0,1},")) 
+      .CHECK_EQUAL(toString(powerset(range(-1.1,2.9))),string("{},{-1.1},{-0.1},{-1.1,-0.1},{0.9},{-1.1,0.9},{-0.1,0.9},{-1.1,-0.1,0.9},{1.9},{-1.1,1.9},{-0.1,1.9},{-1.1,-0.1,1.9},{0.9,1.9},{-1.1,0.9,1.9},{-0.1,0.9,1.9},{-1.1,-0.1,0.9,1.9},"))
+      .CHECK_EQUAL(toString(powerset(chain(oneChar,string("b")))),string("{},{a},{b},{a,b},")) //powerset chains
+      .CHECK_EQUAL(toString(powerset(powerset(chain(oneChar,string("b"))))),string("{},{a},{b},{a,b},")) //powerset of powerset, of another container
 
       .print();
 
-      cout<< "After tests: " << endl;
+      cout<< "After tests: " <<  endl; 
   
   cout << "You have " << tc.right() << " right answers and " << tc.wrong() << " wrong answers so your grade is " << tc.grade() << ". Great!" << endl;
 }
